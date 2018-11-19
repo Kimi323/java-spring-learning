79793141e11462a4ba7ece80cbf600807e7e8a7f
wangmengya <wangmengya2010@yahoo.co.jp>
2018-11-19 16:24:58 +0900

    learn dependency injection: getter and setter injection

--- src/com/luv2code/springdemo/SetterDemoApp.java
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloSpringApp {
+public class SetterDemoApp {
 
 	public static void main(String[] args) {
 		
@@ -12,15 +12,22 @@ public class HelloSpringApp {
 		
 		// retrieve bean from spring container
 		// Coach.class is the interface implemented by all kinds of coach (e.g. TrackCoach class).
-		// myCoach is the beanId defined in applicationContext.xml. since we defined class="com.luv2code.springdemo.BaseballCoach",
-		// spring will create a TrackCoach as the bean.
-		Coach theCoach = context.getBean("myCoach", Coach.class); 
+		// myCoach is the beanId defined in applicationContext.xml. since we defined class="com.luv2code.springdemo.CricketCoach",
+		// spring will create a CricketCoach as the bean.
+		// Coach theCoach = context.getBean("myCricketCoach", Coach.class); 
+		
+		// we cannot use the interface Coach.class if we want to use getEmailAddress() and getTeam() of CricketCoach. 
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		// let's call our new method for fortunes
 		System.out.println(theCoach.getDailyFortune());
+		
+		// call our new methods to get the literal values
+		System.out.println(theCoach.getEmailAddress());
+		System.out.println(theCoach.getTeam());
 		
 		// close the context
 		context.close();
